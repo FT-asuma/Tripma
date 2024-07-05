afafdd2f70b50f75bdc9de46737170735ac06521
Asadulloh <fayzullox512mi@gmail.com>
2024-07-05 22:54:06 +0500

    added insights

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,13 @@ import { Adventures } from "./components/adventures/Adventures";
 import { Entrance } from "./components/entrance/Entrance";
 import { Header } from "./components/header/Header";
 import styles from "./page.module.css";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <CookieAgree />
+      <SpeedInsights/>
       <Entrance />
       <Adventures />
     </main>
