ae66ac4e6ad6b64655d410633d2b7899e2c73c31
Asadulloh <fayzullox512mi@gmail.com>
2024-07-06 17:59:12 +0500

    removed next/font

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import { Button } from "./components/utils/Button";
 import CookieAgree from "./components/PopUps/CookieAgree";
 import { Adventures } from "./components/adventures/Adventures";
 import { Entrance } from "./components/entrance/Entrance";
-import { Header } from "./components/header/Header";
 import styles from "./page.module.css";
 import { SpeedInsights } from "@vercel/speed-insights/next"
 
@@ -10,7 +9,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <CookieAgree />
-      <SpeedInsights/>
+      <SpeedInsights sampleRate={1}/>
       <Entrance />
       <Adventures />
     </main>
